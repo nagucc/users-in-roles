21a421e1cfdf439df281ae4ec077d6b4aff4ccb5
na57 <na57@qq.com>
2018-05-11 22:52:51 +0800

    add apps模块

---
+++ src/app.mjs
@@ -0,0 +1,41 @@
+import mongodb from 'mongodb';
+import { error, info } from './config.mjs';
+
+export default class MongoApply {
+  constructor(mongoUrl, collectionName = 'uir_apps') {
+    this.mongoUrl = mongoUrl;
+    this.collectionName = collectionName;
+  }
+  async getDb() {
+    const db = await mongodb.MongoClient.connect(this.mongoUrl);
+    info('connected to ', this.mongoUrl);
+    db.on('error', (e) => {
+      error(e);
+    });
+    return db;
+  }
+  // 添加申请
+  async insert(appId, name, other) {
+    if (!appId || !name) throw new Error('appId or name must be provided.');
+    const db = await this.getDb();
+    const col = db.collection(this.collectionName);
+    const result = await col.insertOne({
+      ...other,
+      appId,
+      name,
+    });
+    info('insertApp Result:', result.insertedId);
+    const uirId = result.insertedId;
+    db.close();
+    return uirId;
+  }
+
+  // 添加申请
+  async list() {
+    const db = await this.getDb();
+    const col = db.collection(this.collectionName);
+    const result = await col.find().toArray();
+    db.close();
+    return result;
+  }
+}
--- src/index.mjs
+++ src/index.mjs
@@ -1,7 +1,9 @@
 import MongoUserInRole from './users-in-roles.mjs';
 import MongoApply from './apply.mjs';
+import MongoApps from './app.mjs';
 
 export default {
   UserInRole: MongoUserInRole,
   Apply: MongoApply,
+  Apps: MongoApps,
 };
